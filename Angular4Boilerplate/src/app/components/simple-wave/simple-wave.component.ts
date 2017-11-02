7dcb8d0874932f8343ac2b9c36a907d850d77edf
borkovski <borkovski@o2.pl>
2017-11-02 01:11:27 +0100

    Simple wave - second wave added

--- Angular4Boilerplate/src/app/components/simple-wave/simple-wave.component.ts
+++ Angular4Boilerplate/src/app/components/simple-wave/simple-wave.component.ts
@@ -14,18 +14,23 @@ export class SimpleWaveComponent {
     period: number = 0;
     speed: number = 0;
     distance: number = 0;
+    angle2: number = 0;
+    distance2 = Math.random();
 
     reset(canvas: Canvas2d) {
         this.canvas = canvas;
         var canvasDimensions = canvas.getCanvasDimensions();
         this.angle = 0;
+        this.angle2 = 0;
         this.period = 10;
         this.speed = .1;
         this.distance = .5;
+        this.distance2 = Math.random();
     }
 
     tick(canvas: Canvas2d) {
         var currentAngle = this.angle;
+        var currentAngle2 = this.angle;
         var ctx = canvas.getContext();
         ctx.fillStyle = "white";
         ctx.fillRect(0, 0, canvas.getCanvasDimensions().x, canvas.getCanvasDimensions().y);
@@ -33,8 +38,9 @@ export class SimpleWaveComponent {
         ctx.fillStyle = "black";
         ctx.lineWidth = 5;
         for (var x = 0; x < canvas.getCanvasDimensions().x; x += this.period) {
-            var y = this.map(Math.sin(currentAngle), -1, 1, 0, canvas.getCanvasDimensions().y);
+            var y = this.map(Math.sin(currentAngle) + Math.sin(currentAngle2), -2, 2, 0, canvas.getCanvasDimensions().y);
             currentAngle = (currentAngle + this.distance) % (2 * Math.PI);
+            currentAngle2 = (currentAngle2 + this.distance2) % (2 * Math.PI);
             if (x == 0) {
                 ctx.moveTo(x, y);
             }
@@ -45,6 +51,7 @@ export class SimpleWaveComponent {
         ctx.stroke();
         ctx.closePath();
         this.angle += this.speed;
+        this.angle2 += this.speed;
     }
 
     map(currentValue: number, minValue: number, maxValue: number, minMappedValue: number, maxMappedValue: number): number {
